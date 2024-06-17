58740f7d397165e2e28e183e98cc27738437f918
Glenben Ngugi <ngugiglen19@gmail.com>
2024-06-17 16:51:16 +0300

    second commit

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    //
+    public function index(){
+        return view("role-permission.permission.index");
+    }
+
+    public function create(){
+        
+    }
+
+    public function update(){
+        
+    }
+
+    public function destroy(){
+         
+    }
+
+    public function edit(){
+        
+    }
+}
